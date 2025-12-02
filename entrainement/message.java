a814a9462bd4a9b8611c0fbd29f8e4ada1db07e3
Siclemon <simon.jeckert@laposte.net>
2025-12-02 16:43:25 +0100

    nom

---
+++ entrainement/message.java
@@ -0,0 +1,19 @@
+package entrainement;
+
+import java.util.Scanner;
+
+public class message {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String VotreNom;
+
+        System.out.println("Quel est votre nom ?");
+        VotreNom = sc.nextLine();
+        System.out.println("Bonjour " + VotreNom + ", enchanté !");
+
+        sc.close();
+
+    }
+}
