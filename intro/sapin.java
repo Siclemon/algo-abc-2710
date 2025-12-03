3c090ce1ad996dabed2d5b913203951a81555d99
Siclemon <simon.jeckert@laposte.net>
2025-12-03 21:31:58 +0100

    Cimier choisissable

--- intro/sapin.java
+++ intro/sapin.java
@@ -1,33 +1,56 @@
 package intro;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class sapin {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int taille;
-    int tronc;
+            Scanner sc = new Scanner(System.in);
+            Random rng = new Random();
+            int taille;
+            int tronc;
+            String choixPointe;
+            char pointe = '^';
+            
+            System.out.print("Entrer la taille du sapin : ");
+            taille = sc.nextInt();
+            
+            do {
+                System.out.print("Voulez vous un cimier ? (oui/non) ");
+                choixPointe = sc.next();
+            } while (!choixPointe.equals("oui") && !choixPointe.equals("non"));
+            
+            
+            
+            //pointe du sapin
+            if(choixPointe.equals("oui")) {
+                if(taille <= 5) pointe = '*';
+                if(5 < taille && taille <= 11) pointe = 'x';
+                if(taille > 11) pointe = 'X';
+            } else pointe = '^';
+            
+            System.out.println(" ".repeat(taille) + pointe);
+            
+            
+            //branchage
+            for (int i = 1; i <= taille; i++) {
+                
+                System.out.println(" ".repeat(taille-i) + "/".repeat(i)+"|" + "\\".repeat(i));
+                
+            }
+            
+            tronc = taille/4;
+            tronc = tronc % 2 + tronc;
+            
+            //tronc
+            if (taille < 5) {
+                System.out.println(" ".repeat(taille)+"|");
+            }else {
+                for (int i = 0; i < taille/8+1; i++) {
+                    System.out.println(" ".repeat(taille-tronc/2)+"|".repeat(tronc+1));
+                }
+            }
 
-    System.out.print("Entrer la taille du sapin : ");
-    taille = sc.nextInt();
 
-    for (int i = 1; i <= taille; i++) {
-
-        System.out.println(" ".repeat(taille-i) + "/".repeat(i)+"|" + "\\".repeat(i));
-
-    }
-
-    tronc = taille/4;
-    tronc = tronc % 2 + tronc;
-
-
-    if (taille < 5) {
-            System.out.println(" ".repeat(taille)+"|");
-    }else {
-        for (int i = 0; i < taille/8+1; i++) {
-            System.out.println(" ".repeat(taille-tronc/2)+"|".repeat(tronc+1));
-        }
-    }
-
-    sc.close();
+            sc.close();
 }}
