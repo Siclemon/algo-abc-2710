369bb4e9bd7354d131f2822a2500cdf370abce27
Siclemon <simon.jeckert@laposte.net>
2025-12-03 19:40:12 +0100

    Adaptation du tronc à la taille du sapin

--- intro/sapin.java
+++ intro/sapin.java
@@ -6,8 +6,9 @@ public class sapin {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int taille;
+    int tronc;
 
-    System.out.println("Entrer la taille du sapin : ");
+    System.out.print("Entrer la taille du sapin : ");
     taille = sc.nextInt();
 
     for (int i = 1; i <= taille; i++) {
@@ -16,11 +17,15 @@ public class sapin {
 
     }
 
+    tronc = taille/4;
+    tronc = tronc % 2 + tronc;
+
+
     if (taille < 5) {
             System.out.println(" ".repeat(taille)+"|");
     }else {
         for (int i = 0; i < taille/8+1; i++) {
-            System.out.println(" ".repeat(taille-1)+"|||");
+            System.out.println(" ".repeat(taille-tronc/2)+"|".repeat(tronc+1));
         }
     }
 
