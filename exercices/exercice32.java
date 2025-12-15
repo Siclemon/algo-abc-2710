3673f5993ca4a06690ce327afd4325a5b9db348e
Siclemon <simon.jeckert@laposte.net>
2025-12-15 21:54:01 +0100

    exo 3.3

--- exercices/exercice32.java
+++ exercices/exercice32.java
@@ -1,10 +1,11 @@
-package exercices;
+//package exercices;
 import java.util.Scanner;
 public class exercice32 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int s, magasins=0;
 
+        System.out.println("Combien d'argent a Barnabé ?");
         s = sc.nextInt();
 
         while (s>0){
@@ -13,7 +14,7 @@ public class exercice32 {
             else s=0;
 
         }
-        System.out.println(magasins);
+        System.out.println("Barnabé a fait ses courses dans "+magasins+" magasin(s).");
         sc.close();
     }
     
---
+++ exercices/exercice33.java
@@ -0,0 +1,28 @@
+//package exercices;
+import java.util.Scanner;
+public class exercice33 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, i;
+        int[] tableau = {1,2,3,4,5,6,7,8,9,10,15,45,56,4815};
+        boolean trouvé=false;
+
+        System.out.print("Valeur à chercher : ");
+        n = sc.nextInt();
+
+        for (i=0; i<tableau.length; i++) {
+            if (tableau[i] == n) {
+                trouvé = true;
+                break;
+            }
+            if (tableau[i]>n) break;
+        }
+
+        System.out.println();
+        if(trouvé) System.out.println(n+" est présent en position "+i);
+        else System.out.println("404 Not Found");
+
+        sc.close();
+    }
+    
+}
